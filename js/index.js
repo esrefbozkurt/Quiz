0c5613201dc116b0e58b4cb5a8ece3e6bc8eeeff
Esref Bozkurt <esref.bozkurt@gmx.de>
2022-11-04 15:43:08 +0100

    add toggle darkmode, show answer, bookmark

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 // console.clear();
 
-// const bodyElement = document.querySelector('[data-js="body"]');
+//Dark-mode
 
-// const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
-// darkModeButton.addEventListener('click', () => {
-//   bodyElement.classList.add('dark');
+//const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+darkModeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
 //   console.log('switch to dark mode');
 // });
 
@@ -17,3 +20,27 @@
 // const toggleModeButton = document.querySelector('[data-js="toggle-button"]');
 // toggleModeButton.addEventListener('click', () => {
 //   bodyElement.classList.toggle('dark');
+
+// Bookmark Toggle
+
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const bookmarkUnfilled = document.querySelector(
+  '[data-js="bookmark-unfilled"]'
+);
+const bookmarkFilled = document.querySelector('[data-js="bookmark-filled"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkUnfilled.classList.toggle("hidden");
+  bookmarkFilled.classList.toggle("hidden");
+});
+
+//Answer Button Show answer
+
+const showAnswerButton = document.querySelector(
+  '[data-js="show-answer-button"]'
+);
+const showAnswer = document.querySelector('[data-js="show-answer"]');
+
+showAnswerButton.addEventListener("click", () => {
+  showAnswer.classList.toggle("hidden");
+});
