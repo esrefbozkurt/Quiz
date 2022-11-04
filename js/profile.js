2510e75e6ca0018222c1f841c86f190cd27cdae9
Esref Bozkurt <esref.bozkurt@gmx.de>
2022-11-04 18:43:10 +0100

    style form css profile css

---
+++ js/profile.js
@@ -0,0 +1,12 @@
+console.clear();
+//Dark-mode
+
+const nightIcon = document.querySelector('[data-js="nighticon"]');
+const darkIcon = document.querySelector('[ data-js="darkicon"]');
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  nightIcon.classList.toggle("hidden");
+  darkIcon.classList.toggle("hidden");
+});
