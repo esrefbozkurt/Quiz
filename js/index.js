3217f574167ba5d910ce6375bf4f4f8f7f182268
Esref Bozkurt <esref.bozkurt@gmx.de>
2022-11-10 15:53:14 +0100

    changed css background styles/colors add form js update form.html index-user_profile.html js/index.js

--- js/index.js
+++ js/index.js
@@ -33,24 +33,3 @@ const showAnswer = document.querySelector('[data-js="show-answer"]');
 showAnswerButton.addEventListener("click", () => {
   showAnswer.classList.toggle("hidden");
 });
-
-//add quesrtion card
-
-const body = document.querySelector("body");
-
-const questionInput = document.querySelector('[data-js="your-question"]');
-const answerInput = document.querySelector('[data-js="your-answer"]');
-const tagInput = document.querySelector('[data-js="your-tag"]');
-const submitButton = document.querySelector('[data-js="submit-button"]');
-
-submitButton.addEventListener("submit", (event) => {
-  const newQuizcard = document.createElement("article");
-  newQuizcard.classList.add("quiz__card");
-  body.append(newQuizcard);
-
-  const myContent = document.createElement("p");
-  myContent.classList.add("post__content");
-  myContent.textContent =
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
-  myPost.append(myContent);
-});
